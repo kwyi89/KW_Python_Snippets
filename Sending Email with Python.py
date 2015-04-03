b147c2f89524d1c048e2765737ede67f122dcc5a
kwyi89 <kwyi89@gmail.com>
2015-04-02 20:21:12 -0700

    Updating email script

--- Sending Email with Python.py
+++ Sending Email with Python.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[3]:
 
 # account setup
 username = '***';
@@ -50,5 +50,72 @@ def send_email(smtp_address, usr, password, msg, mode):
         exit()
     server.quit()
     
-### compose email    
+# compose email  
+# takes all the details for an email and sends it
+# address format: list, [0] - to
+#                       [1] - cc
+#                       [2] - bcc
+# subject format: string
+# body format: list of pairs [0] - text
+#                            [1] - type:
+#                                        0 - plain
+#                                        1 - html
+# files is list of strings
+def compose_email(addresses, subject, body, files):
+    
+    # addresses
+    to_address = addresses[0]
+    cc_address = addresses[1]
+    bcc_address = addresses[2]
+    
+    # create a message
+    msg = create_msg(to_address, cc_address=cc_address, subject=subject)
+    
+    # add text
+    for text in body:
+        attach_text(msg, text[0], text[1])
+        
+    if (files != ''):
+        file_list = files.split(',')
+        for afile in file_list:
+            attach_file(msg, afile)
+            
+    # send message
+    send_email(server, username, password, msg, 0)
+    
+    # check for bcc
+    if (bcc_address != ''):
+        msg['Bcc'] = bcc_address
+        send_email(server, username, password, msg, 1)
+        
+    print 'email sent'
+    
+    
+# attach text
+# attaches a plain text or html text to a message
+def attach_text(msg, atext, mode):
+    part = MIMEText(atext, get_mode(mode))
+    msg.attach(part)
+    
+def get_mode(mode):
+    if (mode == 0):
+        mode = 'plain'
+    elif (mode == 1):
+        mode = 'html'
+    else:
+        print 'error in text kind'
+        print 'email cancelled'
+    return mode
+
+# attach file
+# takes the msg and a file name and attaches the file to the message
+def attach_file(msg, afile):
+    part = MIMEApplication(open(afile, "rb").read())
+    part.add_header('Content-Disposition', 'attachment', filename=afile)
+    msg.attach(part)
+
+
+# In[ ]:
+
+
 
